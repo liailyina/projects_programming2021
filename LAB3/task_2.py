e215ac625c2da57fb207b76988a70612c9f670c7
liailyina <liailyinaa@gmail.com>
2021-08-19 10:33:21 +0300

    Added window

--- LAB3/task_2.py
+++ LAB3/task_2.py
@@ -11,12 +11,15 @@ screen = pygame.display.set_mode((400, 400))
 rect(screen, COFFEE_BROWN, (1, 1, 399, 399))
 rect(screen, BROWN, (1, 1, 399, 190))
 
+# Window
+rect(screen, DOUBLE_LIGHT_BLUE, (220, 15, 160, 160))
+rect(screen, VERY_LIGHT_BLUE, (220, 10, 160, 15))
+rect(screen, VERY_LIGHT_BLUE, (220, 10, 15, 165))
+rect(screen, VERY_LIGHT_BLUE, (220, 170, 160, 15))
+rect(screen, VERY_LIGHT_BLUE, (365, 10, 15, 165))
+rect(screen, VERY_LIGHT_BLUE, (295, 10, 15, 165))
+rect(screen, VERY_LIGHT_BLUE, (220, 50,160, 15))
 
-
-
-
-
-pygame.display.update()
 
 while True:
     for event in pygame.event.get():
